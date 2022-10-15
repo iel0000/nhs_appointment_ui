c8657e6a488b269f94a3e49bba2d2f9e29f61250
gcube02 <tayag.jomari02@gmail.com>
2022-10-15 11:57:32 +0800

    update

---
+++ src/app/shared/primeng.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { StyleClassModule } from 'primeng/styleclass';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { BadgeModule } from 'primeng/badge';
+import { SidebarModule } from 'primeng/sidebar';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { RippleModule } from 'primeng/ripple';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { AvatarModule } from 'primeng/avatar';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { StepsModule } from 'primeng/steps';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    InputTextModule,
+    PasswordModule,
+    MenubarModule,
+    ButtonModule,
+    StyleClassModule,
+    SidebarModule,
+    RadioButtonModule,
+    InputSwitchModule,
+    RippleModule,
+    AvatarModule,
+    CalendarModule,
+    CheckboxModule,
+    MultiSelectModule,
+    DropdownModule,
+    StepsModule,
+  ],
+  providers: [],
+  exports: [
+    ReactiveFormsModule,
+    HttpClientModule,
+    InputTextModule,
+    PasswordModule,
+    MenubarModule,
+    ButtonModule,
+    StyleClassModule,
+    AvatarGroupModule,
+    BadgeModule,
+    AvatarModule,
+    CalendarModule,
+    CheckboxModule,
+    MultiSelectModule,
+    DropdownModule,
+    StepsModule,
+  ],
+})
+export class PrimeNgModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { PrimeNgModule } from './primeng.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    PrimeNgModule,
+    RouterModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    RouterModule,
+  ],
+  providers: [],
+  exports: [
+    ReactiveFormsModule,
+    PrimeNgModule,
+  ],
+})
+export class SharedModule {}
